725c5567fafbfcdc6d821ec3364bef1d37bd141e
Farshid Shokoohi <farshid.moon@hotmail.com>
2016-04-01 00:29:01 +1300

    Start FoodNow

---
+++ FoodNow/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FoodNow.Startup))]
+namespace FoodNow
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

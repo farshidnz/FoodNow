aff64c5e80a3f8b560f13e23af8922410f5016e0
Farshid Shokoohi <farshid.moon@hotmail.com>
2016-04-03 23:10:23 +1200

    autocomplete doesnt work. search does

--- FoodNow/Controllers/HomeController.cs
+++ FoodNow/Controllers/HomeController.cs
@@ -10,10 +10,20 @@ namespace FoodNow.Controllers
     public class HomeController : Controller
     {
         FoodNowDb _db = new FoodNowDb();
+
+        public ActionResult Autocomplete(string term)
+        {
+            var model = _db.Restaurants.Where(r => r.Suburb.StartsWith(term)).Take(7).Select(r => new { label = r.Suburb });
+            return Json(model, JsonRequestBehavior.AllowGet);
+        }
         public ActionResult Index(string searchTerm = null)
         {
             var model = _db.Restaurants.Where(r => searchTerm == null || r.Suburb.Equals(searchTerm))
                 .Take(10).ToList();
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView("_Restaurants", model);
+            }
             return View(model);
         }
 
---
+++ FoodNow/Scripts/fn.js
@@ -0,0 +1,32 @@
+﻿$(function () {
+    var ajaxFormSubmit = function () {
+        var $form = $(this);
+        var options = {
+            url: $form.attr("action"),
+            type: $form.attr("method"),
+            data: $form.serialize()
+        };
+        $.ajax(options).done(function (data) {
+            var $target = $($form.attr("data-fn-target"));
+            $target.replaceWith(data);
+        });
+        return false;
+    };
+    var submitAutocompleteForm = function (event, ui) {
+        var $input = $(this);
+        $input.val(ui.item.label);
+        var $form = $input.parent("form:first");
+        $form.submit();
+    };
+
+    var createAutocomplete = function () {
+        var $input = $(this);
+        var options = {
+            source: $input.attr("data-fn-autocomplete")
+            //select: submitAutocompleteForm
+        }; 
+        $input.autocomplete(options);
+    };
+    $("form[data-fn-ajax='true'").submit(ajaxFormSubmit);
+    $("input[data-fn-autocomplete]").each(createAutocomplete);
+});
